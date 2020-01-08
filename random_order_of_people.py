334b1f80366ff0153b937a071c1af338bdcb291c
Michael Levy <mike.levy.work@gmail.com>
2020-01-08 14:51:46 -0700

    Update to avoid pylint warnings

--- random_order_of_people.py
+++ random_order_of_people.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
+"""
+    Every meeting a different person from the BGC group will give a brief update on his or her work.
+    Only exists so that we don't need to draw names out of a hat when the cycle is due to repeat.
+"""
+
 import random
 import time
 
+NOW = time.time()
+SEED = int(NOW) % 10000
+print(f'time.time() = {NOW} so seeding with {SEED}')
+random.seed(SEED)
 
-now = time.time()
-seed = int(now) % 10000
-print(f'time.time() = {now} so seeding with {seed}')
-random.seed(seed)
-
-people = ['Magdalena',
+PEOPLE = ['Magdalena',
           'Kristen',
           'Mike',
           'Keith',
@@ -18,6 +22,6 @@ people = ['Magdalena',
           'Dan'
          ]
 
-print(f'\nOriginal list (group sorted alphabetically by last name, I hope)\n{people}')
-random.shuffle(people)
-print(f'\nShuffled list\n{people}')
+print(f'\nOriginal list (group sorted alphabetically by last name, I hope)\n{PEOPLE}')
+random.shuffle(PEOPLE)
+print(f'\nShuffled list\n{PEOPLE}')
