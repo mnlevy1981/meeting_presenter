6edda8feecb5e859b94e26c799f8bbaa723e6288
Michael Levy <mike.levy.work@gmail.com>
2023-11-10 15:25:42 -0700

    Add argparse and reading from stdin

--- random_order_of_people.py
+++ random_order_of_people.py
@@ -9,40 +9,67 @@ import os
 import random
 import time
 
-def _get_participants_from_stdin():
-    participants = ['Magdalena',
-                    'Kristen',
-                    'Mike',
-                    'Keith',
-                    'Matt',
-                    'Precious',
-                    'Dan'
-                   ]
-    return participants
+#####################################
+
+def _parse_args():
+    """ Parse command line arguments
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Shuffle elements of a list",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-c', '--command-line', action='store_true', dest='cmd_line',
+                        help='Flag to force user to enter list from command line')
+
+    parser.add_argument('-l', '--limit-first', action='store_true', dest='lim_first',
+                        help='Flag to limit who goes first or second in new cycle')
+
+    return parser.parse_args()
+
+#####################################
+
+def _get_list_items_from_stdin():
+    """ Read in list to shuffle from standard input
+    """
+    list_items = list()
+    print("Enter a blank line to denote end of the list!")
+    while True:
+        list_items.append(input("Participant name: "))
+        if list_items[-1] == '':
+            print("----")
+            return list_items[:-1]
+
+#####################################
 
 if __name__ == '__main__':
-    # 1) Set up seeding for random number generator used to shuffle names
+    """ Main script to do the shuffling
+    """
+    # 1) Read command line arguments
+    args = _parse_args()
+
+    # 2) Set up seeding for random number generator used to shuffle names
     NOW = time.time()
     SEED = int(NOW) % 10000
     print(f'time.time() = {NOW} so seeding with {SEED}\n----')
     random.seed(SEED)
 
-    # 2) If previous cycle was written to a file, read it back in
-    #    We store previous_cycle_file_exists because it determines if there are restrictions
+    # 3) If previous cycle was written to a file, read it back in
+    #    We store use_previous_cycle_file because it determines if there are restrictions
     #    to the order of the shuffling (e.g. last person can't subsequently go first)
     previous_cycle_file = 'previous_cycle.txt'
-    previous_cycle_file_exists = os.path.isfile(previous_cycle_file)
-    if previous_cycle_file_exists:
+    use_previous_cycle_file = os.path.isfile(previous_cycle_file) and not args.cmd_line
+    if use_previous_cycle_file:
         previous_cycle = open(previous_cycle_file, "r")
         orig_people = previous_cycle.read().splitlines()
         previous_cycle.close()
     else:
-        orig_people = _get_participants_from_stdin()
+        orig_people = _get_list_items_from_stdin()
 
-    # 3) Do the shuffling and print results
+    # 4) Do the shuffling and print results
     people = orig_people.copy()
     random.shuffle(people)
-    if previous_cycle_file_exists:
+    if use_previous_cycle_file and args.lim_first:
         # If we read in the last cycle from a file, we want to
         # limit who can go first or second in the new cycle
         while (orig_people[-1] in people[:2]) or (orig_people[-2] == people[0]):
@@ -51,7 +78,7 @@ if __name__ == '__main__':
     print(f'\nOriginal list\n{orig_people}')
     print(f'\nShuffled list\n{people}')
 
-    # 4) Store cycle in new file
+    # 5) Store cycle in new file
     previous_cycle = open(previous_cycle_file, "w+")
     for person in people:
         previous_cycle.write(f'{person}\n')
